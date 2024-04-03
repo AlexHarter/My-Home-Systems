092de0f097527a2f4f4b559a1393a84bda798523
Alex Harter <alexharter97@gmail.com>
2024-04-03 14:42:25 -0500

    added pseudo install script

---
+++ install-datapad.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Machine Name: alex-datapad
+# - Functions:
+#   - Data Science
+#   - Job Searching
+#   - Family Administration
+
+# Linux Distribution: Void
+# - Install Instructions:
+#   - Filesystems:
+#	- sda1 512M vfat /boot/efi
+#	- sda2 swap
+#	- sda3 remaining memory /
+
+# Backend:
+# - IPC: dbus
+# - Session Manager: elogind
+# - Networking: NetworkManager
+# Terminal Apps:
+# - Backup: timeshift
+# - Text Editor: neovim
+# - File Manager: nnn
+# - Web Browser: lynx
+# - Audio Player
+# - Version Control: git
+# - Root permission: opendoas
+# Desktop:
+# - Display Manager: N/A
+# - Window Manager: i3
+# - Brightness: brightnessctl
+# - Compositor: picom
+# - Status Bar: polybar
+# - Terminal: alacritty
+# - Shell: fish
+# - Wallpaper: feh
+# - App Launcher: rofi
+# - Audio: pipewire
+# GUI Apps:
+# - Text Editor: N/A
+# - File Manager: N/A
+# - Web Browser: qutebrowser
+# - Notetaker: Obsidian (flatpak)
+# Other:
+# - Anaconda (shell install)
