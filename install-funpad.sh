159c5dae90ff2c2c2701d257fe6a4db2214cf14a
Alex <alex@alex-datapad.harter>
2024-04-08 12:10:24 -0500

    final before switching to Debian

---
+++ install-funpad.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Machine Name: alex-funbook
+# - functions:
+#     - Publishing
+#     - Music Production
+#     - Music Notation
+#     - Gaming
+#     - Spending time with brothers
+
+
+
+xbps-install -Suy dbus elogind dbus-elogind polkit-elogind NetworkManager timeshift neovim nnn lynx git opendoas i3 brightnessctl picom polybar alacritty fish feh rofi pipewire qutebrowser flatpak ardour lmms furnace
